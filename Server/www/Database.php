53c7a7e24980eb48284b764ebd6ca8714ceaf53b
Not Mike <notmike@notmike.co.uk>
2019-03-27 16:01:13 +0000

    Add UTF8 encoding to connection string

--- Server/www/Database.php
+++ Server/www/Database.php
@@ -1,11 +1,12 @@
 <?php
 error_reporting(E_ALL);
+ini_set('default_charset', 'UTF8');
 class Database
 {
 	private $connection;
 	function __construct()
 	{
-		$this->connection = new PDO("mysql:dbname=MegaLAN;host=127.0.0.1", "MegaLAN", "MegaLAN");
+		$this->connection = new PDO("mysql:dbname=MegaLAN;host=127.0.0.1;charset=utf8", "MegaLAN", "MegaLAN");
 		$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	function Query($sql, $args = NULL){
