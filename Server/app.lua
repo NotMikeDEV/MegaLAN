753a520c83791dfcc9853ce6d2644e69b35974a9
Not Mike <notmike@notmike.co.uk>
2019-03-28 22:27:20 +0000

    Change to DNS verification for LetsEncrypt validation. Also switch nodejs version to latest. Not that it makes much difference to this project...

--- Server/app.lua
+++ Server/app.lua
@@ -6,22 +6,19 @@ network:AddNameserver("8.8.8.8")
 require("module/caddy")
 require("module/php")
 php.debug=true
-local WS = caddy:AddWebsite{hostname='', port="80", root='/www/redirect'}
-local WS = caddy:AddWebsite{hostname='www.megalan.app', port="80", root='/www/redirect'}
-local WS = caddy:AddWebsite{hostname='megalan.app', port="80", root='/www'}
 local Hostname = exec("hostname", true)
 Hostname = exec("echo -n " .. Hostname, true)
-local Server = caddy:AddWebsite{hostname=Hostname .. '.megalan.app', port="443", root='/www'}
 Mount{path='/www', type="map", source="/MegaLAN/www"}
 Mount{path='/MegaLAN/', type="map", source="/MegaLAN/"}
 
 function install_container()
 	install_package("ca-certificates unbound")
-	exec_or_die("wget -O- https://deb.nodesource.com/setup_8.x | bash -")
+	exec_or_die("wget -O- https://deb.nodesource.com/setup_11.x | bash -")
 	install_package("nodejs")
 	exec_or_die("npm install mysql")
 	exec_or_die("npm install sendmail")
 	exec_or_die("npm install ip6addr")
+	write_file("etc/Cloudflare", read_file("/etc/Cloudflare"))
 	return 0
 end
 
@@ -33,11 +30,58 @@ server:
 		access-control: 127.0.0.1/32 allow
 		access-control: ::1/128 allow
 ]])
+	write_file("/etc/Caddyfile", [[
+:80 {
+	root /www/redirect
+	fastcgi / /run/php/php7.0-fpm.sock {
+			ext     .php
+			split   .php
+			index   index.php
+	}
+}
+
+]] .. Hostname .. [[.megalan.app:443 {
+	root /www
+	fastcgi / /run/php/php7.0-fpm.sock {
+			ext     .php
+			split   .php
+			index   index.php
+	}
+	log /var/log/caddy.log {
+			rotate_size 5
+			rotate_age  14
+			rotate_keep 20
+	}
+	tls {
+		dns cloudflare
+	}
+	header / Strict-Transport-Security "max-age=31536000;"
+}
+
+megalan.app:443 {
+	root /www
+	fastcgi / /run/php/php7.0-fpm.sock {
+		ext     .php
+		split   .php
+		index   index.php
+	}
+	log /var/log/caddy.log {
+		rotate_size 5
+		rotate_age  14
+		rotate_keep 20
+	}
+	tls {
+		dns cloudflare
+	}
+	header / Strict-Transport-Security "max-age=31536000;"
+}
+]])
 	return 0
 end
 
 function background()
-	exec("unbound")
+	exec("cd /root && caddy -conf /etc/Caddyfile -env /etc/Cloudflare -email notmike@notmike.co.uk -agree &")
+	exec("unbound &")
 	exec("cd /MegaLAN && while true; do ./UDPServer.js " .. Hostname .. ".megalan.app; sleep 1; done&")
 	exec("cd /MegaLAN && while true; do ./HTTPServer.js; sleep 1; done&")
 	return 0
