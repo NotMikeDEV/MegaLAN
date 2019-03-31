4b9f2185b9e9e879ad0952cb708ce05b83f56d35
Not Mike <notmike@notmike.co.uk>
2019-03-31 16:11:27 +0100

    Added internal DNS server.

--- Server/DNSServer.js
+++ Server/DNSServer.js
@@ -1,56 +1,136 @@
 #!/usr/bin/nodejs
 var dns = require('native-dns');
+const IPAddr = require('ip6addr');
 var args = process.argv.slice(2);
 var ServerName = args[0];
+var DomainName = args[1];
 
-var IPv4 = false;
-var IPv6 = false;
+var IPv4 = args[2];
+var IPv6 = args[3];
+
+var mysql = require('mysql');
+var database = mysql.createPool({
+	connectionLimit: 5,
+	host: 'localhost',
+	user: 'MegaLAN',
+	password: 'MegaLAN',
+	database: 'MegaLAN'
+});
+
+setInterval(function () {
+	if (IPv4 && IPAddr.parse(IPv4))
+		database.query("REPLACE INTO Servers (ServerName, IP, HeartBeatTime) VALUES (?, ?, ?)", [ServerName, IPv4, Math.floor(new Date())], function (err) { if (err) console.log(err); });
+	if (IPv6 && IPAddr.parse(IPv6))
+		database.query("REPLACE INTO Servers (ServerName, IP, HeartBeatTime) VALUES (?, ?, ?)", [ServerName, IPv6, Math.floor(new Date())], function (err) { if (err) console.log(err); });
+	database.query("DELETE FROM Servers WHERE HeartBeatTime < ?", [Math.floor(new Date()) - 90000], function (err) { if (err) console.log(err); });
+	database.query("DELETE FROM DNS WHERE Expire < ?", [Math.floor(new Date())], function (err) { if (err) console.log(err); });
+}, 60000);
+database.query("REPLACE INTO Servers (ServerName, IP, HeartBeatTime) VALUES (?, ?, ?)", [ServerName, IPv4, Math.floor(new Date())], function (err) { if (err) console.log(err); });
+database.query("REPLACE INTO Servers (ServerName, IP, HeartBeatTime) VALUES (?, ?, ?)", [ServerName, IPv6, Math.floor(new Date())], function (err) { if (err) console.log(err); });
+database.query("DELETE FROM DNS WHERE Expire < ?", [Math.floor(new Date())], function (err) { if (err) console.log(err); });
 
 var DNSHandler = function (request, response) {
-	console.log("DNS Request", request);
 	var question = request.question[0];
 	var hostname = question.name.toLowerCase();
 	var Host = hostname.split(".");
-
-	response.answer.push(dns.SOA({
-		name: hostname,
-		ttl: 300,
-		primary: 'MegaLAN.app',
-		admin: 'MegaLAN',
-		serial: 1,
-		refresh: 600,
-		retry: 600,
-		expiration: 3600,
-		minimum: 5,
-	}));
-	response.authority.push(dns.NS({
-		name: hostname,
-		data: ServerName,
-		ttl: 5,
-	}));
-	dns.resolve(ServerName, 'A', function (err, IPv4) {
-		dns.resolve(ServerName, 'AAAA', function (err, IPv6) {
-			if (IPv4.length) {
-				for (var x in IPv4) {
-					response.answer.push(dns.A({
-						name: request.question[0].name,
-						address: IPv4[x],
-						ttl: 5,
-					}));
-				}
-			}
-			if (IPv6.length) {
-				for (var x in IPv6) {
-					response.answer.push(dns.AAAA({
-						name: request.question[0].name,
-						address: IPv6[x],
-						ttl: 5,
-					}));
-				}
-			}
+	console.log("DNS Request", Host, question.type, request.address.address);
+	response.header.aa = true;
+	if (hostname == DomainName || question.type == 6)
+	{
+		response.answer.push(dns.SOA({
+			name: hostname,
+			ttl: 300,
+			primary: ServerName + "." + DomainName,
+			admin: 'MegaLAN',
+			serial: 1,
+			refresh: 600,
+			retry: 600,
+			expiration: 3600,
+			minimum: 5,
+		}));
+	}
+	if (Host[0] == "_acme-challenge" && question.type == 16) {
+		database.query("SELECT Hostname, Type, Value, Expire FROM DNS WHERE Hostname = ? AND Type = 16", [hostname], function (err, Data) {
+			if (err) console.log(err);
+			for (x in Data)
+				response.answer.push(dns.TXT({
+					name: hostname,
+					data: [Data[x].Value],
+					ttl: 3,
+				}));
 			response.send();
 		});
-	});
+	}
+	else if (question.type == 1 || question.type == 28 || question.type == 2 || question.type == 255) {
+		database.query("SELECT ServerName, IP FROM Servers WHERE ServerName = ? ORDER BY RAND()", [Host[0]], function (err, result) {
+			if (result.length) {
+				for (var x in result) {
+					var IP = IPAddr.parse(result[x].IP);
+					if (IP.kind() == 'ipv4')
+						response.answer.push(dns.A({
+							name: hostname,
+							address: IP.toString(),
+							ttl: 300,
+						}));
+					if (IP.kind() == 'ipv6')
+						response.answer.push(dns.AAAA({
+							name: hostname,
+							address: IP.toString(),
+							ttl: 300,
+						}));
+				}
+				response.send();
+			}
+			else {
+				database.query("SELECT ServerName, IP FROM Servers", function (err, result) {
+					for (var x in result) {
+						var IP = IPAddr.parse(result[x].IP);
+						if (IP.kind() == 'ipv4' && (question.type == 1 || question.type == 255)) {
+							response.answer.push(dns.A({
+								name: hostname,
+								address: IP.toString(),
+								ttl: 30,
+							}));
+							response.additional.push(dns.A({
+								name: result[x].ServerName + "." + DomainName,
+								address: IP.toString(),
+								ttl: 300,
+							}));
+						}
+						if (IP.kind() == 'ipv6' && (question.type == 28 || question.type == 255)) {
+							response.answer.push(dns.AAAA({
+								name: hostname,
+								address: IP.toString(),
+								ttl: 30,
+							}));
+							response.additional.push(dns.AAAA({
+								name: result[x].ServerName + "." + DomainName,
+								address: IP.toString(),
+								ttl: 300,
+							}));
+						}
+						if (question.type == 2)
+							response.answer.push(dns.NS({
+								name: hostname,
+								data: result[x].ServerName + "." + DomainName,
+								ttl: 300,
+							}));
+						else
+							response.authority.push(dns.NS({
+								name: hostname,
+								data: result[x].ServerName + "." + DomainName,
+								ttl: 300,
+							}));
+					}
+					response.send();
+				});
+			}
+		});
+	}
+	else
+	{
+		response.send();
+	}
 };
 var ErrorHandler = function (err, buff, req, res) {
 	console.log(err.stack);
@@ -59,19 +139,43 @@ var UDP4 = dns.createServer({ dgram_type: 'udp4' }).on('request', DNSHandler).on
 var TCP4 = dns.createTCPServer({ dgram_type: 'tcp4' }).on('request', DNSHandler).on('error', ErrorHandler);
 var UDP6 = dns.createServer({ dgram_type: 'udp6' }).on('request', DNSHandler).on('error', ErrorHandler);
 var TCP6 = dns.createTCPServer({ dgram_type: 'tcp6' }).on('request', DNSHandler).on('error', ErrorHandler);
-dns.resolve(ServerName, 'A', function (err, IPv4) {
-	for (var x in IPv4)
-		console.log("DNS Server UDP", IPv4[x], UDP4.serve(53, IPv4[x]));
-});
-dns.resolve(ServerName, 'A', function (err, IPv4) {
-	for (var x in IPv4)
-		console.log("DNS Server TCP", IPv4[x], TCP4.serve(53, IPv4[x]));
-});
-dns.resolve(ServerName, 'AAAA', function (err, IPv6) {
-	for (var x in IPv6)
-		console.log("DNS Server UDP6", IPv6[x], UDP6.serve(53, IPv6[x]));
-});
-dns.resolve(ServerName, 'AAAA', function (err, IPv6) {
-	for (var x in IPv6)
-		console.log("DNS Server TCP6", IPv6[x], TCP6.serve(53, IPv6[x]));
-});
+console.log("DNS Server UDP", IPv4, UDP4.serve(53, IPv4));
+console.log("DNS Server TCP", IPv4, TCP4.serve(53, IPv4));
+console.log("DNS Server UDP6", IPv6, UDP6.serve(53, IPv6));
+console.log("DNS Server TCP6", IPv6, TCP6.serve(53, IPv6));
+
+var RFC2136Handler = function (request, response) {
+	var question = request.question[0];
+	var hostname = question.name.toLowerCase();
+	console.log("RFC2136", question);
+	response.answer.push(dns.SOA({
+		name: hostname,
+		ttl: 300,
+		primary: 'MegaLAN',
+		admin: 'MegaLAN',
+		serial: 1,
+		refresh: 600,
+		retry: 600,
+		expiration: 3600,
+		minimum: 5,
+	}));
+	if (request.authority && request.authority.length)
+	{
+		for (var x in request.authority)
+		{
+			if (request.authority[x].ttl && request.authority[x].class == 1)
+			{
+				database.query("REPLACE INTO DNS (Hostname, Type, Value, Expire) VALUES (?, ?, ?, ?)", [request.authority[x].name, request.authority[x].type, request.authority[x].data, Math.floor(new Date()) + (request.authority[x].ttl * 1000)], function (err) {
+					if (err)
+						console.log(err);
+					else
+						response.send();
+				});
+			}
+		}
+	}
+};
+var RFC2136UDP = dns.createUDPServer({ dgram_type: 'udp4' }).on('request', RFC2136Handler).on('error', ErrorHandler);
+console.log("RFC2136 UDP", RFC2136UDP.serve(99, "127.0.0.1"));
+var RFC2136TCP = dns.createTCPServer({ dgram_type: 'tcp4' }).on('request', RFC2136Handler).on('error', ErrorHandler);
+console.log("RFC2136 TCP", RFC2136TCP.serve(99, "127.0.0.1"));
--- Server/app.lua
+++ Server/app.lua
@@ -5,9 +5,16 @@ network:AddNameserver("9.9.9.9")
 network:AddNameserver("8.8.8.8")
 require("module/caddy")
 require("module/php")
-php.debug=true
-local Hostname = exec("hostname -f", true)
-Hostname = exec("echo -n " .. Hostname, true)
+
+local ServerName = exec("hostname -s", true)
+ServerName = exec("echo -n " .. ServerName, true)
+local DomainName = exec("hostname -d", true)
+DomainName = exec("echo -n " .. DomainName, true)
+local IPv4 = exec([[ip addr|grep "inet "|grep global|cut -d " " -f 6|cut -d "/" -f 1|head -n 1]], true)
+IPv4 = exec("echo -n " .. IPv4, true)
+local IPv6 = exec([[ip addr|grep "inet6 "|grep global|cut -d " " -f 6|cut -d "/" -f 1|head -n 1]], true)
+IPv6 = exec("echo -n " .. IPv6, true)
+
 Mount{path='/www', type="map", source="/MegaLAN/www"}
 Mount{path='/MegaLAN/', type="map", source="/MegaLAN/"}
 
@@ -19,7 +26,6 @@ function install_container()
 	exec_or_die("npm install sendmail")
 	exec_or_die("npm install ip6addr")
 	exec_or_die("npm install native-dns")
-	write_file("etc/Cloudflare", read_file("/etc/Cloudflare"))
 	return 0
 end
 
@@ -41,7 +47,7 @@ server:
 	}
 }
 
-]] .. Hostname .. [[:443 {
+]] .. ServerName .. "." .. DomainName .. [[:443 {
 	root /www
 	fastcgi / /run/php/php7.0-fpm.sock {
 			ext     .php
@@ -54,12 +60,12 @@ server:
 			rotate_keep 20
 	}
 	tls {
-		dns cloudflare
+		dns rfc2136
 	}
 	header / Strict-Transport-Security "max-age=31536000;"
 }
 
-megalan.app:443 {
+]] .. DomainName .. [[:443 {
 	root /www
 	fastcgi / /run/php/php7.0-fpm.sock {
 		ext     .php
@@ -72,7 +78,7 @@ megalan.app:443 {
 		rotate_keep 20
 	}
 	tls {
-		dns cloudflare
+		dns rfc2136
 	}
 	header / Strict-Transport-Security "max-age=31536000;"
 }
@@ -81,10 +87,10 @@ megalan.app:443 {
 end
 
 function background()
-	exec("cd /root && caddy -conf /etc/Caddyfile -env /etc/Cloudflare -email notmike@notmike.co.uk -agree &")
 	exec("unbound &")
-	exec("cd /MegaLAN && while true; do ./UDPServer.js " .. Hostname .. "; sleep 1; done&")
-	exec("cd /MegaLAN && while true; do ./DNSServer.js " .. Hostname .. "; sleep 1; done&")
+	exec("cd /MegaLAN && while true; do ./UDPServer.js " .. ServerName .. "." .. DomainName .. "; sleep 1; done&")
+	exec("cd /MegaLAN && while true; do ./DNSServer.js " .. ServerName .. " " .. DomainName .. " " .. IPv4 .. " " .. IPv6 .. "; sleep 1; done&")
+	exec("export RFC2136_NAMESERVER=127.0.0.1:99; sleep 2; cd /root && while true; do caddy -conf /etc/Caddyfile -email notmike@notmike.co.uk -agree; sleep 1; done&")
 	exec("cd /MegaLAN && while true; do ./HTTPServer.js; sleep 1; done&")
 	return 0
 end
