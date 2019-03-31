b6b56265665684f43a551494478d72f70d032257
Not Mike <notmike@notmike.co.uk>
2019-03-31 16:12:17 +0100

    Remove silly line.

--- Server/www/index.php
+++ Server/www/index.php
@@ -4,7 +4,6 @@ $Title = "Home Page";
 include("Header.php");
 ?>
 <p class="lead">MegaLAN is an easy to use free p2p VPN client.</p>
-<p class="lead">Just install, log in, select a network, and connect!</p>
 <p>/!\</p>
 <p><A HREF="https://github.com/NotMikeDEV/MegaLAN/blob/master/MegaLAN-64bit.exe?raw=true" class="btn btn-lg btn-secondary">Download 64bit Installer</A></p>
 <p><A HREF="https://github.com/NotMikeDEV/MegaLAN/blob/master/MegaLAN-32bit.exe?raw=true" class="btn btn-lg btn-secondary">Download 32bit Installer</A></p>
