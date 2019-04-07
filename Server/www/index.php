c5d8be447b0eb28aedf20471ebc631709e8783ae
Not Mike <notmike@notmike.co.uk>
2019-04-07 02:03:19 +0100

    Add Linux Instructions link.

--- Server/www/index.php
+++ Server/www/index.php
@@ -6,7 +6,8 @@ include("Header.php");
 <a href="https://github.com/NotMikeDEV/MegaLAN" target="_blank"><img width="149" height="149" src="/icons/forkme_left_orange_ff7600.png" style="position:absolute; top:0; left:0;" alt="Fork me on GitHub" data-recalc-dims="1"></a>
 <p class="lead">MegaLAN is an easy to use free p2p VPN client.</p>
 <p>/!\</p>
-<p><A HREF="https://github.com/NotMikeDEV/MegaLAN/blob/master/MegaLAN-64bit.exe?raw=true" class="btn btn-lg btn-secondary">Download 64bit Installer</A></p>
-<p><A HREF="https://github.com/NotMikeDEV/MegaLAN/blob/master/MegaLAN-32bit.exe?raw=true" class="btn btn-lg btn-secondary">Download 32bit Installer</A></p>
+<p><A HREF="https://github.com/NotMikeDEV/MegaLAN/blob/master/MegaLAN-64bit.exe?raw=true" class="btn btn-lg btn-secondary">Download 64bit Windows Installer</A></p>
+<p><A HREF="https://github.com/NotMikeDEV/MegaLAN/blob/master/MegaLAN-32bit.exe?raw=true" class="btn btn-lg btn-secondary">Download 32bit Windows Installer</A></p>
+<p><A HREF="https://github.com/NotMikeDEV/MegaLAN/blob/master/Linux/README.md" class="btn btn-lg btn-secondary">Linux Instructions</A></p>
 <?php
 include("Footer.php");
